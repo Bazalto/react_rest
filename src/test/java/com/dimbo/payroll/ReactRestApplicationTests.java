aa065a0b04ff9205ac9124222087ebc1c7aa88ff
Dmitiriy Cherkashyn <dmitriy.cherkashyn@gmail.com>
2017-07-03 19:25:02 +0300

    lots

---
+++ src/test/java/com/dimbo/payroll/ReactRestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.dimbo.payroll;
+
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class ReactRestApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
